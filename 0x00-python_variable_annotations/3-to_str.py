9be8a36417a3a931e929a24b427ee50c410bc553
Adewole Conde <phatboislym@protonmail.com>
2023-03-30 14:45:45 +0100

    add: task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module for a float to str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    args: n: float
+    return: string: str
+    """
+    string: str = str(n)
+    return (string)
