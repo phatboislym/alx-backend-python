b5eef4d54928c34b31c5d534f7510d7822ae7a39
Adewole Conde <phatboislym@protonmail.com>
2023-03-30 14:45:56 +0100

    add: task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a module that adds type annotation for some variables
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
