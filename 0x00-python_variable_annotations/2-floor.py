f7c74696ce9c122a7a5252d54cdde4170b95c7d4
Adewole Conde <phatboislym@protonmail.com>
2023-03-30 14:51:46 +0100

    fix: add documentation for module and function

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+"""
+module for a function that casts a float to an int
+"""
+
 
 def floor(n: float) -> int:
+    """
+    args: n: float
+    return: x: int
+    """
     x: int = int(n)
     return (x)
