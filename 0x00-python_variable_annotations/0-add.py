cd8015558a92d7d54894ae8b8a4886179915eb76
Adewole Conde <phatboislym@protonmail.com>
2023-03-30 10:20:59 +0100

    add: task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+add function with type annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    args: float: a, b
+    return: float: c
+    """
+    c: float = (a + b)
+    return (c)
