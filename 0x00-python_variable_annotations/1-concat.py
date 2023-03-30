76a07807f5f5f0480add96a5812f8321e2843646
Adewole Conde <phatboislym@protonmail.com>
2023-03-30 14:54:02 +0100

    fix: lint fix to remove W291 warning

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -7,7 +7,7 @@ module for a string concat function
 def concat(str1: str, str2: str) -> str:
     """
     args: str: str1, str2
-    return: str: concat_str 
+    return: str: concat_str
     """
     concat_str: str = f"{str1}{str2}"
     return (concat_str)
