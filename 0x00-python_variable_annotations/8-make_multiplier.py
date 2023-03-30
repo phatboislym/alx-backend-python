aa8d925e669bccc5a48b452d46907bdf85306131
Adewole Conde <phatboislym@protonmail.com>
2023-03-30 20:51:53 +0100

    fix: documentation positioning

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from typing import Callable
 """
 module for type-annotated higher-order function make_multiplier
 takes a float multiplier as argument
 returns a function that multiplies a float by multiplier
 """
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
