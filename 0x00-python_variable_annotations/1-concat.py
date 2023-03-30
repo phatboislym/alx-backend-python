03111f70215e49acfd1332e83e71c6b2cbacd937
Adewole Conde <phatboislym@protonmail.com>
2023-03-30 14:45:02 +0100

    add: task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module for a string concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    args: str: str1, str2
+    return: str: concat_str 
+    """
+    concat_str: str = f"{str1}{str2}"
+    return (concat_str)
