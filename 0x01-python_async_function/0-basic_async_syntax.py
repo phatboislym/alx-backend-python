088d2baf0486aed0932c61e26bc2fbfcd1d9a6ce
Adewole Conde <phatboislym@protonmail.com>
2023-04-03 23:19:09 +0100

    fix: change param for random.uniform to int

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -17,6 +17,6 @@ async def wait_random(max_delay: int = 10) -> float:
     args: max_delay: int
     return: random_delay: float
     """
-    random_delay = uniform(0.0, max_delay)
+    random_delay = uniform(0, max_delay)
     await sleep(random_delay)
     return (random_delay)
