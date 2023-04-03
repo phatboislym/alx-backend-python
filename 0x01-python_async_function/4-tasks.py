a6f92c28010be39dbae423ee9d9350fdf20f641f
Adewole Conde <phatboislym@protonmail.com>
2023-04-03 23:04:45 +0100

    add: task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+module for an async routine `task_wait_n`
+takes two integer arguments (n, max_delay)
+spawns `task_wait_random` n times with the specified max_delay
+return the list of all the asyncio.Task objects
+import task_wait_random from 3-tasks.py
+"""
+
+from asyncio import sleep
+from typing import Any, List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[Any]:
+    """
+    args: n: int, max_delay: int
+    return: list_delay: List[Any]
+    """
+    list_tasks = []
+    for i in range(n):
+        list_tasks.append(await task_wait_random(max_delay))
+        await sleep(0.1)
+    list_tasks = sorted(list_tasks)
+    return (list_tasks)
