1bc06c37aae35a34c58f94848a8ef816d62e4678
Adewole Conde <phatboislym@protonmail.com>
2023-04-25 00:02:50 +0100

    add: task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,8 @@ contains a `TestAccessNestedMap` class that inherits from unittest.TestCase
 from parameterized import parameterized
 from typing import Mapping, Sequence, Union
 from unittest import TestCase
-from utils import access_nested_map
+from unittest.mock import MagicMock, Mock as mock, patch
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(TestCase):
@@ -23,8 +24,13 @@ class TestAccessNestedMap(TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
-                               expected_result: Union[int, str, dict, list]) -> None:
+    def test_access_nested_map(self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               expected_result: Union[int,
+                                                      str,
+                                                      dict,
+                                                      list]) -> None:
         """
         args:   self
                 nested_map: Mapping
@@ -48,3 +54,30 @@ class TestAccessNestedMap(TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """
+    test class for the get_json function
+    methods:    TestGetJson.test_get_json
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url: str, test_payload: dict,
+                      mock_get: MagicMock) -> None:
+        """
+        args:   self
+                test_url: str
+                test_payload: dict
+                mock_get: MagicMock
+        return: None
+        """
+
+        mock_get.return_value = mock(json=lambda: test_payload)
+        response = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(response, test_payload)
